52353789a23dea78559715ad6aa25ea92f701a81
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-02-04 20:51:26 +0100

    Get, post, patch and delete method

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const fs = require("fs");
+
+const app = express();
+const port = 8000;
+app.use(express.json());
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+app.get("/api/v1/tours", (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        },
+    })
+})
+
+app.get("/api/v1/tours/:id", (req, res) => {
+    const id = req.params.id * 1;
+
+    const tour = tours.find((el) => el.id === id)
+
+    // if (id > tours.length)
+    if (!tour) {
+        return res.status(404).json({
+            status: 'failed',
+            message: 'Invalid ID',
+        })
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour,
+        },
+    })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body)
+
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour,
+            }
+        })
+    })
+})
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'failed',
+            message: 'Invalid ID',
+        })
+    }
+
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+});

b3f36ac05ac48b1d59a8f885e7af9de3df297f5c
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-02-06 21:28:19 +0100

    connection to mongo database

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+const mongoose = require('mongoose')
 const app = require('./app');
 const dotenv = require('dotenv')
 
 dotenv.config({
     path: './.env'
 })
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('DB connexion successfull');
+});
 
 // STARTING SERVER
 const port = process.env.PORT;

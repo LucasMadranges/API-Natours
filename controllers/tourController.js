a9486888f91fb25243bf12781d2baf6c40e996a3
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-02-07 21:11:42 +0100

    new Class for features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,6 @@
 const {Tour} = require('./../models/tourModel');
 const {query} = require("express");
+const {APIFeatures} = require('./../utils/apiFeatures')
 
 function aliasTopTours(req, res, next) {
     req.query.limit = '5';
@@ -10,48 +11,9 @@ function aliasTopTours(req, res, next) {
 
 async function getAllTours(req, res) {
     try {
-        // BUILD QUERY
-        const queryObj = {...req.query}
-        const excludeFields = ['page', 'sort', 'limit', 'fields'];
-        excludeFields.forEach(el => delete queryObj[el])
-
-        // ADVANCED FILTERING
-        let queryStr = JSON.stringify(queryObj)
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
-
-        let query = Tour.find(JSON.parse(queryStr));
-
-        // SORTING
-        if (req.query.sort) {
-            const sortBy = req.query.sort.split(',').join(' ')
-            query = query.sort(sortBy);
-        } else {
-            query = query.sort('-createdAt');
-        }
-
-        // FIELDS LIMITING
-        if (req.query.fields) {
-            const fields = req.query.fields.split(',').join(' ');
-            query = query.select(fields)
-        } else {
-            query = query.select('-__v')
-        }
-
-        // PAGINATION
-        const page = +req.query.page || 1;
-        const limit = +req.query.limit || 100;
-        const skip = (page - 1) * limit;
-
-        if (req.query.page) {
-            const numTours = await Tour.countDocuments();
-
-            if (skip >= numTours) throw new Error('This page does not exist');
-        }
-
-        query = query.skip(skip).limit(limit);
-
         // EXECUTE QUERY
-        const tours = await query;
+        const features = new APIFeatures(Tour.find(), req.query).filter().sort().limit().paginate();
+        const tours = await features.query;
 
         // SEND RESPONSE
         res.status(200).json({

8e9ea9fc5298c699d80509fa443bab976d3ca19e
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-02-11 17:49:45 +0100

    aggregate middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -84,13 +84,18 @@ tourSchema.pre('save', function (next) {
 tourSchema.pre(/^find/, function (next) {
     this.find({secretTour: {$ne: true}})
     this.start = Date.now();
-
     next()
 })
 
 tourSchema.post(/^find/, function (res, next) {
     console.log(`Query took ${Date.now() - this.start} milliseconds`)
-    console.log(res)
+    next()
+})
+
+// AGGREGATION MIDDLEWARE
+
+tourSchema.pre('aggregate', function (next) {
+    this.pipeline().unshift({$match: {secretTour: {$ne: true}}})
     next()
 })
 
